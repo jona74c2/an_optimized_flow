c0839e9f5b3d9f823ff3cf32b38b49dc565a1082
oscarbagger <oscarbagger@live.dk>
2020-04-24 01:23:46 +0200

    database setup

--- modules/settings.js
+++ modules/settings.js
@@ -1,6 +1,6 @@
 export function settingsObject() {
   const settings = {};
-  settings.endpoint = "";
-  settings.apiKey = "";
+  settings.endpoint = "https://keaassignment-e242.restdb.io/rest/dxc-users";
+  settings.apiKey = "5ea209a6919b360ce59b7c7b";
   return settings;
 }
