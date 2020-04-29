e9f25e85230083d565bf628f0e6d729952bf6f9c
Jonas Friis Kjær <jona74c2@stud.kea.dk>
2020-04-29 14:31:54 +0200

    code cleaning in videohandler

--- videoHandler.js
+++ videoHandler.js
@@ -11,14 +11,12 @@ function start() {
   let video = document.querySelector("#splashvid source");
   let videocontainer = document.querySelector("#splashvid");
   if (window.screen.width > 900) {
-    console.log(video);
     video.src = videoSource.desktop;
     videocontainer.load();
-    //videocontainer.setAttribute('poster', newposter); //Changes video poster image
     videocontainer.play();
-    console.log(videoSource.desktop);
-    console.log(video);
   } else {
     video.src = videoSource.mobile;
+    videocontainer.load();
+    videocontainer.play();
   }
 }
