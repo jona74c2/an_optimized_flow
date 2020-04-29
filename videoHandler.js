e9a1c42eb16ce7ac7f0ad13f838f2e5e862a3303
Jonas Friis Kjær <jona74c2@stud.kea.dk>
2020-04-29 14:31:05 +0200

    video is now changed due to screen width

--- videoHandler.js
+++ videoHandler.js
@@ -2,15 +2,22 @@
 window.addEventListener("DOMContentLoaded", start);
 
 const videoSource = {
-  mobile: "static/dxcmobile.mp4",
-  square: "static/dxcsquare.mp4",
-  desktop: "static/dxcdesktop.mp4",
+  mobile: "/dxcmobile.mp4",
+  square: "/dxcsquare.mp4",
+  desktop: "/dxcdesktop.mp4",
 };
 
 function start() {
   let video = document.querySelector("#splashvid source");
+  let videocontainer = document.querySelector("#splashvid");
   if (window.screen.width > 900) {
+    console.log(video);
     video.src = videoSource.desktop;
+    videocontainer.load();
+    //videocontainer.setAttribute('poster', newposter); //Changes video poster image
+    videocontainer.play();
+    console.log(videoSource.desktop);
+    console.log(video);
   } else {
     video.src = videoSource.mobile;
   }
