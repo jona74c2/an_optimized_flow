81dedd09c323aa192d1851d2b9b096bb6c7c4790
oscarbagger <oscarbagger@live.dk>
2020-04-29 02:09:06 +0200

    desktop first draft

---
+++ videoHandler.js
@@ -0,0 +1,10 @@
+"use strict";
+window.addEventListener("DOMContentLoaded", start);
+
+const videoSource = {
+  mobile: "/static/dxcmobile.mp4?autoplay=1&loop=1",
+  square: "/static/dxcsquare.mp4?autoplay=1&loop=1",
+  desktop: "/static/dxcdesktop.mp4?autoplay=1&loop=1",
+};
+
+function start() {}
